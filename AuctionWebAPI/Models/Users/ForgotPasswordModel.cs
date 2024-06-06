f278c9d61a75fdff77692c9715cdc571a6aae211
quangquac <quang28102003@gmail.com>
2024-06-06 08:55:45 +0700

    JWT Registration/Login/Authentication. Forgot Password and Remember Password In processing ...

---
+++ AuctionWebAPI/Models/Users/ForgotPasswordModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionWebAPI.Models.Users
+{
+    public class ForgotPasswordModel
+    {
+        [Required, EmailAddress, Display(Name = "Registered email address")]
+        public string Email { get; set; }
+        public bool EmailSent { get; set; }
+    }
+}
---
+++ AuctionWebAPI/Models/Users/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace AuctionWebAPI.Models.Users
+{
+    public class UserDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Address { get; set; }
+    }
+}
