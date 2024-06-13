817973dc0957cf3ed2ee0aac389a418106ccd3b6
phngminh <minhphamph0600@gmail.com>
2024-06-14 03:53:22 +0700

    Create AuctionView.jsx

---
+++ FrontEnd/src/components/AuctionView/AuctionView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Upcoming from './Upcoming'
+
+const AuctionView = () => {
+    return (
+        <div className="main-auction">
+            <Upcoming />
+        </div>
+    )
+}
+
+export default AuctionView
