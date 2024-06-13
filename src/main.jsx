3b92bc61b7e8bc820de2753a203f7752a47fd317
phngminh <minhphamph0600@gmail.com>
2024-06-14 02:03:58 +0700

    Revert "Update main.jsx"
    This reverts commit c8f32b2a10e7cca65b145e056fa0928d707ef8a7.

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
