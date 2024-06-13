ae957d260d1a69365732c957d60c31a7082a92ca
phngminh <minhphamph0600@gmail.com>
2024-06-14 03:53:20 +0700

    Update Users.jsx

--- FrontEnd/src/components/AdminView/Users.jsx
+++ FrontEnd/src/components/AdminView/Users.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../layouts/Users.css';
+import '../Layouts/Users.css';
 
 const Users = () => {
   const navigate = useNavigate();
