e5cc7f01605e7f55da1b67ec62603e993d5a9d44
phngminh <minhphamph0600@gmail.com>
2024-06-14 18:25:09 +0700

    Fix

--- FrontEnd/src/components/AdminView/Dashboard.jsx
+++ FrontEnd/src/components/AdminView/Dashboard.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import PieChart from '../Charts/PieChart.jsx';
 import LineChart from '../Charts/LineChart.jsx';
 import Sidebar from './Sidebar.jsx';
 import '../Layouts/Dashboard.css';
 
 const Dashboard = () => {
-  const navigate = useNavigate();
   return (
     <div className="dashboard">
       <Sidebar />
@@ -14,13 +12,6 @@ const Dashboard = () => {
         <main className="main-content">
           <h1>Dashboard</h1>
           <h2>Website Statisctic</h2>
-          
-          <div className="toolbar">
-            <button onClick={() => navigate('/*')} className="nav-button">Main</button>
-            <button onClick={() => navigate('/users')} className="nav-button">Users</button>
-            <button onClick={() => navigate('/histories')} className="nav-button">Histories</button>
-          </div>
-          
           <div className="charts">
             <PieChart />
             <LineChart />
