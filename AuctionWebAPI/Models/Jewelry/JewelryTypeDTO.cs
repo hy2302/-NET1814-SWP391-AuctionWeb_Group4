cd3ee1f2a0ba8bde7a54141ef973a293108b73e1
dtthinh <dotruongthinh2212@gmail.com>
2024-06-19 09:12:51 +0700

    Revert "Create Crud all table in Database, Create Validation for decimal price, update UserDTO, apply new DTO class for CRUD operation"
    This reverts commit 11e57f46a279155e159a63f6fef32152d49c192c.

--- AuctionWebAPI/Models/Jewelry/JewelryTypeDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuctionWebAPI.Models.Jewelry
-{
-    public class JewelryTypeDTO
-    {
-        public int? JewelryTypeId { get; set; }
-        public string? JewelryTypeName { get; set; }
-    }
-}
